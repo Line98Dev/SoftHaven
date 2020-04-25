f840361b45fafd297d9b975c437eebd254ab89df
evanwitous@gmail.com <evanwitous@gmail.com>
2020-04-25 16:23:16 -0400

    Added ship bean for Agent/Custom Officer use

---
+++ PortCall/src/main/java/beans/Ship.java
@@ -0,0 +1,60 @@
+package main.java.beans;
+
+public class Ship implements Cloneable {
+
+	private String name;
+	private int imo;
+	private int berth;
+	private String state;
+	
+	public Ship(String name, int imo, int berth, String state) {
+		this.name = name;
+		this.imo = imo;
+		this.berth = berth;
+		this.state = state;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	
+	public void setIMO(int imo) {
+		this.imo = imo;
+	}
+	
+	public int getIMO() {
+		return this.imo;
+	}
+	
+	
+	public void setBerth(int berth) {
+		this.berth = berth;
+	}
+	
+	public int getBerth() {
+		return this.berth;
+	}
+	
+	
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public String getState() {
+		return this.state;
+	}
+	
+	
+	public Ship cloneMe() {
+		try {
+			return (Ship) super.clone();
+		} catch (CloneNotSupportedException e) {
+			return null ;
+		}
+	}
+}
